138dc8a65afc5baac099c9003ce2bfc2d74975c3
Mikumyluvv <127746975+Mikumyluvv@users.noreply.github.com>
2023-05-25 09:34:04 +0700

    Update

---
+++ Latihan Modul 5/Node.java
@@ -0,0 +1,18 @@
+/**
+ * Project COBA5
+ * author Ryuu
+ */
+
+
+
+public class Node {
+    int data;
+    Node left;
+    Node right;
+
+    public Node(int data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
